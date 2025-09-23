987d5f130342e4fb3ec3a153679697646dd0e36c
Hải Tân <221124022310@due.udn.vn>
2025-09-23 22:41:00 +0700

    Hai commit v31

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKCKOYHJHCsjwiIQrfPebFOaCiBRoh7xY",
-  authDomain: "my-portfolio-77be9.firebaseapp.com",
-  projectId: "my-portfolio-77be9",
-  storageBucket: "my-portfolio-77be9.firebasestorage.app",
-  messagingSenderId: "660005631580",
-  appId: "1:660005631580:web:e236663411cab2ec7e9899",
-  measurementId: "G-EQD4W7SLRZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
